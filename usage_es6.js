f0e93c4d690bdf8a1d92a5af52705883aef6cdf0
iceyangcc <iceyangcc@163.com>
2017-12-08 12:38:59 +0800

    bug fixd

---
+++ usage_es6.js
@@ -0,0 +1,20 @@
+
+/**
+ * ES6使用指南
+ * IDValidator.isValid(idCard)
+ */
+
+ import IDValidator from './id_validator_es6'
+
+ const id1 = '320311770706001'
+ const id2 = '32031177070600X'
+ const id3 = '230102197911069897'
+ const id4 = '230102197911069890'
+
+ console.log(IDValidator.isValid(id1))
+
+ console.log(IDValidator.isValid(id2))
+
+ console.log(IDValidator.isValid(id3))
+
+ console.log(IDValidator.isValid(id4))
